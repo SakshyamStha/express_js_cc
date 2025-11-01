f32a0331aef2a0c42cd5cabd0b627beaa8658ff9
SakshyamStha <sakshamstha6785@gmail.com>
2025-11-01 11:56:24 +0545

    static routing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 // we use this app variable everywhere to create routes, apis, etc...
@@ -7,8 +8,12 @@ app.listen(8000, ()=> console.log("Server is running in port 8000"));
 
 app.get('/',(req,res)=>{
     // res.send("k xaaa") 
-    res.send({
-        message: "jserguisb gjkrbguwsb",
-        test: "hjgrbsb"
-    }) 
+    // res.send({
+    //     message: "jserguisb gjkrbguwsb",
+    //     test: "hjgrbsb"
+    // }) 
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+} )
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 } )
